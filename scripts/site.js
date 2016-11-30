1db2948f6658329c19f9e0385328c8a1816c447e
Brian Grinstead <briangrinstead@gmail.com>
2016-11-30 13:17:00 -0800

    check in build files

---
+++ scripts/site.js
@@ -0,0 +1,26 @@
+
+function loadExample() {
+	
+	$("section.viewsource").each(function() {
+		var el = $(this),
+			pre = $('<pre class="html"></pre>').text(el.html());
+			
+		el.append(pre);
+	});
+	
+	$("style").each(function(i, el) {
+		var style = $(this),
+			linkedTo = style.attr("data-linked"),
+			pre = $('<pre class="css">' + style.html() + '</pre>');
+		
+		if (linkedTo) {
+			$('#' + linkedTo).append(pre)
+		}
+		else {
+			$('#example').append(pre);
+		}
+	});
+
+}
+
+$(loadExample);
